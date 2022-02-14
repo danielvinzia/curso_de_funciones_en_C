0b0dc943572c3f5a1953792c1848f4c4365ac0ec
danielvinzia <danielvinzia@gmail.com>
2022-02-14 10:36:06 -0300

    clase de principales bibliotecas de C concluida

---
+++ biblioteca_estandar_de_funciones/06-principales_bibliotecas_de_C.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // INPUT Y OUTPUT
+#include <conio.h> // COMUNICARNOS CON LA CONSOLA
+#include <string.h> // CADENAS DE CARACTERES
+#include <stdlib.h> // LIBRERIA BIEN ESTANDAR, MUY UTIL
+#include <math.h> // FUNCIONES MATEMATICAS
+#include <time.h> // FECHAS Y TIEMPO, ZONAS HORARIAS
+#include <ctype.h> // MANEJO DE CARACTERES INDIVIDUALES
+#include <signal.h> // MANEJO DE SEÑALES, DETECTAR DEL ENTORNO
+#include <locale.h> // DEFINIR CUESTIONES DE LOCACION
+#include <errno.h> // NO AYUDA A MANEJAR ERRORES
+#include <assert.h> // NOS DA UNA MACRO, E IMPREME UN DIAGNOSTICO DE LO QUE ESTA PASANDO
+
