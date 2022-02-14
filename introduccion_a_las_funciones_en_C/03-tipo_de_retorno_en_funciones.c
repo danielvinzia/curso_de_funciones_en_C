9f34bf7476cd97f8e0d0007cdc96576b774dae11
danielvinzia <danielvinzia@gmail.com>
2022-02-14 09:44:06 -0300

    clase de tipo de retorno en funciones concluida

---
+++ introduccion_a_las_funciones_en_C/03-tipo_de_retorno_en_funciones.c
@@ -0,0 +1,31 @@
+// Funciones sin argumentos y sin valor de retorno
+/*
+void functionName();
+
+functionName();
+
+void functionName()
+{
+    // logica
+}
+*/
+
+// Funciones con argumentos pero sin valores a retornar
+/*
+void functionName1(float);
+
+functionName1(float);
+
+void functionName1(float)
+{
+    // logica
+}
+*/
+
+// funciones sin argumentos pero que si tiene valores a retornar
+
+float functionName2();
+
+// funciones con argumentos con return value
+
+float functionName(int, float);
