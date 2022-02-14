25528d093de788f523b7d7c435afa72f9dc2f3f8
danielvinzia <danielvinzia@gmail.com>
2022-02-14 09:10:43 -0300

    clase de que es una funcion concluida

---
+++ introduccion_a_las_funciones_en_C/01-que_es_una_funcion_en_C.c
@@ -0,0 +1,13 @@
+// ASI ES LA ESTRUCTURA DE UNA FUNCION
+/*
+return-type funtion_name(parameter declarations, if any)
+{
+    declaration
+    statements
+}
+*/
+
+int powerOfIntegers(int base, int n)
+{
+    
+}
